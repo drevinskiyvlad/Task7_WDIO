02e0107f19290d88901913cb10adc60b2b731f8d
Drevinskyi Vladislav <drevinskiy.vlad@gmail.com>
2023-12-15 00:40:22 +0200

    created data and helper

---
+++ test/helper/faker.js
@@ -0,0 +1,21 @@
+exports.Faker = class Faker{
+    static generateRandomString(length) {
+        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            result += characters.charAt(randomIndex);
+        }
+        return result;
+    }
+
+    static generateRandomNumberString(length) {
+        const characters = '123456789';
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            result += characters.charAt(randomIndex);
+        }
+        return result;
+    }
+}
